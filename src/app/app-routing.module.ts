ef43cb8e3e2903ba277a4b70cb7b38fa861f5be8
godhanidenis <godhanidenis@gmail.com>
2023-05-03 11:31:18 +0530

    Create basic layout.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/welcome' },
+  // { path: '', pathMatch: 'full', redirectTo: '/sidebar' },
+  { path: 'welcome', loadChildren: () => import('./pages/welcome/dashboard.module').then(m => m.DashboardModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
