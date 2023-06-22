059781cbd13fef43c6592b7c20e2f29d798ff6cb
denisgodhani <godhanidenis@gmail.com>
2023-06-22 19:27:26 +0530

    reorder template type

--- src/app/components/add-edit-multiple-products/add-edit-multiple-products.component.ts
+++ src/app/components/add-edit-multiple-products/add-edit-multiple-products.component.ts
@@ -19,6 +19,22 @@ export class AddEditMultipleProductsComponent implements OnInit {
   isUploadVisible: boolean = false;
   chooseType = [
     {
+      label: 'Edit Unit Price',
+      value: 'EDIT_PRICE',
+    },
+    {
+      label: 'Edit Shipping Dimensions',
+      value: 'EDIT_SHIPPING_DIMENSIONS',
+    },
+    {
+      label: 'Edit Product Details',
+      value: 'EDIT_PRODUCT_DETAILS',
+    },
+    {
+      label: 'Change Product Status',
+      value: 'CHANGE_PRODUCT_STATUS',
+    },
+    {
       label: 'Edit MPN',
       value: 'EDIT_MPN',
     },
@@ -31,28 +47,12 @@ export class AddEditMultipleProductsComponent implements OnInit {
       value: 'ADD_EDIT_UPC',
     },
     {
-      label: 'Edit Price',
-      value: 'EDIT_PRICE',
-    },
-    {
-      label: 'Edit Shipping Dimensions',
-      value: 'EDIT_SHIPPING_DIMENSIONS',
-    },
-    {
-      label: 'Change Product Status',
-      value: 'CHANGE_PRODUCT_STATUS',
-    },
-    {
       label: 'Remove ASIN',
       value: 'REMOVE_ASIN',
     },
     {
       label: 'Remove UPC',
       value: 'REMOVE_UPC',
-    },
-    {
-      label: 'Edit Product Details',
-      value: 'EDIT_PRODUCT_DETAILS',
     },
     // 'ADD_PRODUCT',
   ];
@@ -72,14 +72,19 @@ export class AddEditMultipleProductsComponent implements OnInit {
     userPermissionService.userPermission.subscribe((permission: any) => {
       this.userPermissions = permission;
       if (this.userPermissions.partner_map) {
-        this.chooseType.push({
+        this.chooseType.splice(1, 0, {
           label: 'Add/Edit MAP',
           value: 'ADD_EDIT_MAP',
         });
-      }
-      if (this.userPermissions.partner_sku_level_handling) {
-        this.chooseType.push({
-          label: 'SKU specific Handling Time ',
+        if (this.userPermissions.partner_sku_level_handling) {
+          this.chooseType.splice(2, 0, {
+            label: 'SKU specific Handling Time',
+            value: 'EDIT_SKU_SPECIFIC_HANDLING_TIME',
+          });
+        }
+      } else {
+        this.chooseType.splice(1, 0, {
+          label: 'SKU specific Handling Time',
           value: 'EDIT_SKU_SPECIFIC_HANDLING_TIME',
         });
       }
