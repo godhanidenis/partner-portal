ead26e2f048adf32bef42d578c0507cd52657cc6
denisgodhani <godhanidenis@gmail.com>
2023-06-29 16:39:59 +0530

    testing zendesk

--- src/app/components/zendesk-sso/zendesk-sso.component.ts
+++ src/app/components/zendesk-sso/zendesk-sso.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Route, Router } from '@angular/router';
 import { NzMessageService } from 'ng-zorro-antd/message';
 
 @Component({
@@ -8,9 +9,17 @@ import { NzMessageService } from 'ng-zorro-antd/message';
   styleUrls: ['./zendesk-sso.component.scss'],
 })
 export class ZendeskSSOComponent {
+
+  url = "https://flyontechsolutions.zendesk.com/access/jwt?jwt=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2ODgwMzY4NDAsImp0aSI6IjFkYzBjZjY4LTE2NmQtMTFlZS1hOTljLTMwMjQzMjdiZjU3ZSIsIm5hbWUiOiJEZW5pcyIsImVtYWlsIjoiZGdvZGhhbmkxODAyQGdtYWlsLmNvbSJ9.thJe4kEyKZDyd4Ywh_qrSIgCDzaesojgEnB8OZaHxfQ"
   constructor(
     private httpClient: HttpClient,
-    private message: NzMessageService
-  ) {}
-  ngOnInit(): void {}
+    private message: NzMessageService,
+    private router: Router,
+  ) {
+
+  }
+  ngOnInit(): void {
+    alert('Okay I am processding for auth');
+    window.location.href = this.url;
+  }
 }
