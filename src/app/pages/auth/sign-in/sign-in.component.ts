48c6a15115aae05bb1cd2171bf08c8b71ed728db
godhanidenis <godhanidenis@gmail.com>
2023-07-03 18:00:28 +0530

    Create redirac flow.

--- src/app/pages/auth/sign-in/sign-in.component.ts
+++ src/app/pages/auth/sign-in/sign-in.component.ts
@@ -5,7 +5,7 @@ import {
   FormGroup,
   Validators,
 } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { NzMessageService } from 'ng-zorro-antd/message';
 import { LoginReq, LoginRes } from 'src/app/shared/model/auth.model';
 import { AuthService } from 'src/app/shared/service/auth.service';
@@ -20,13 +20,21 @@ export class SignInComponent implements OnInit {
   isLoading: boolean = false;
   passwordShow: boolean = true;
   submitError: boolean = false;
+  paramsObject: any = '';
 
   constructor(
     private fb: FormBuilder,
     private router: Router,
     private message: NzMessageService,
-    private authService: AuthService
-  ) {}
+    private authService: AuthService,
+    private activatedRoute: ActivatedRoute
+  ) {
+    this.activatedRoute.queryParams.subscribe((params: Params) => {
+      console.log(params);
+      this.paramsObject = params;
+    });
+    // http://localhost:4200/auth/login?brand_id=16482819213329&locale_id=1&return_to=https:%2F%2Fsupport.123stores.com%2Fhc%2Fen-us&timestamp=1688387072
+  }
 
   ngOnInit(): void {
     this.loginForm = this.fb.group({
@@ -49,14 +57,24 @@ export class SignInComponent implements OnInit {
       };
       this.authService.login(dataReq).subscribe(
         (result: any) => {
-          console.log(result);
           this.isLoading = false;
           if (result.success) {
             this.message.success('User login successfully!!');
             this.authService.setAccessToken(result.access_token);
             this.authService.setRefreshToken(result.refresh_token);
             this.authService.saveUser(result.user_profile);
-            this.router.navigate(['/main/dashboard']);
+            if (
+              this.paramsObject.return_to ===
+              'https://support.123stores.com/hc/en-us'
+            ) {
+              //  var objectUrl = res.template_url;
+              var a = document.createElement('a');
+              //  a.download = 'document';
+              a.href = this.paramsObject.return_to;
+              a.click();
+            } else {
+              this.router.navigate(['/main/dashboard']);
+            }
           }
         },
         (err) => (this.isLoading = false)
